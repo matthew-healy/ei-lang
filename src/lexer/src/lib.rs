99e678583fb6d13c47146200fd0ae2fad2752303
Matthew Healy <matthew.healy@soundcloud.com>
2021-03-26 17:34:29 +0100

    Parse two-character tokens

--- src/lexer/src/lib.rs
+++ src/lexer/src/lib.rs
@@ -1,61 +1,84 @@
+use std::iter::Peekable;
+
 #[derive(Debug, PartialEq)]
 enum TokenKind {
-    LeftBrace,  // {
-    RightBrace, // }
-    LeftParen,  // (
-    RightParen, // )
-    Dot,        // .
-    Colon,      // :
-    SemiColon,  // ;
-    Bang,       // !
-    Plus,       // +
-    Minus,      // -
-    Star,       // *
-    Slash,      // /
-    Equal,      // =
-    Greater,    // >
-    Less,       // <
+    LeftBrace,    // {
+    RightBrace,   // }
+    LeftParen,    // (
+    RightParen,   // )
+    Dot,          // .
+    Colon,        // :
+    SemiColon,    // ;
+    Bang,         // !
+    BangEqual,    // !=
+    Plus,         // +
+    Minus,        // -
+    Star,         // *
+    Slash,        // /
+    Equal,        // =
+    EqualEqual,   // ==
+    Greater,      // >
+    Less,         // <
+    LessEqual,    // <=
+    GreaterEqual, // >=
+    RightArrow,   // ->
+    And,          // &&
+    Or,           // ||
 }
 
-struct TokenStream<I> {
-    src: I,
+struct TokenStream<I: Iterator<Item=char>> {
+    src: Peekable<I>,
 }
 
 impl <I: Iterator<Item=char>> Iterator for TokenStream<I> {
-   type Item = TokenKind;
+    type Item = TokenKind;
 
-   fn next(&mut self) -> Option<TokenKind> {
-       match self.src.next() {
-           Some(c) => parse_token(c),
-           None => None
-       }
-   }
-}
-
-fn parse_token(c: char) -> Option<TokenKind> {
-    use TokenKind::*;
-    match c {
-        '{' => Some(LeftBrace),
-        '}' => Some(RightBrace),
-        '(' => Some(LeftParen),
-        ')' => Some(RightParen),
-        '.' => Some(Dot),
-        ':' => Some(Colon),
-        ';' => Some(SemiColon),
-        '!' => Some(Bang),
-        '+' => Some(Plus),
-        '-' => Some(Minus),
-        '/' => Some(Slash),
-        '*' => Some(Star),
-        '=' => Some(Equal),
-        '>' => Some(Greater),
-        '<' => Some(Less),
-        _ => None
+    fn next(&mut self) -> Option<TokenKind> {
+        self.next_token_kind()
     }
 }
 
-fn token_stream<I>(src: I) -> TokenStream<I> where I: Iterator<Item=char> {
-    TokenStream { src }
+impl <I: Iterator<Item=char>> TokenStream<I> {
+    fn next_token_kind(&mut self) -> Option<TokenKind> {
+        use TokenKind::*;
+        let next = self.src.next();
+        match next {
+            Some('{') => Some(LeftBrace),
+            Some('}') => Some(RightBrace),
+            Some('(') => Some(LeftParen),
+            Some(')') => Some(RightParen),
+            Some('.') => Some(Dot),
+            Some(':') => Some(Colon),
+            Some(';') => Some(SemiColon),
+            Some('!') if self.consume('=') => Some(BangEqual),
+            Some('!') => Some(Bang),
+            Some('+') => Some(Plus),
+            Some('-') if self.consume('>') => Some(RightArrow),
+            Some('-') => Some(Minus),
+            Some('/') => Some(Slash),
+            Some('*') => Some(Star),
+            Some('=') if self.consume('=') => Some(EqualEqual),
+            Some('=') => Some(Equal),
+            Some('>') if self.consume('=') => Some(GreaterEqual),
+            Some('>') => Some(Greater),
+            Some('<') if self.consume('=') => Some(LessEqual),
+            Some('<') => Some(Less),
+            Some('&') if self.consume('&') => Some(And),
+            Some('|') if self.consume('|') => Some(Or),
+            _ => None
+        }
+    }
+
+    fn consume(&mut self, c: char) -> bool {
+        if self.src.peek() == Some(&c) {
+           self.src.next();
+           true
+        } else { false }
+    }
+}
+
+fn token_stream<I: Iterator<Item=char>>(src: I) -> TokenStream<I> {
+    TokenStream { src: src.peekable() }
 }
 
 #[cfg(test)]
@@ -70,27 +93,37 @@ mod tests {
     }
 
     #[test_with_parameters(
-        [ input, expected              ]
-        [ "{"  , TokenKind::LeftBrace  ]
-        [ "}"  , TokenKind::RightBrace ]
-        [ "("  , TokenKind::LeftParen  ]
-        [ ")"  , TokenKind::RightParen ]
-        [ "."  , TokenKind::Dot        ]
-        [ ":"  , TokenKind::Colon      ]
-        [ ";"  , TokenKind::SemiColon  ]
-        [ "!"  , TokenKind::Bang       ]
-        [ "+"  , TokenKind::Plus       ]
-        [ "-"  , TokenKind::Minus      ]
-        [ "*"  , TokenKind::Star       ]
-        [ "/"  , TokenKind::Slash      ]
-        [ "="  , TokenKind::Equal      ]
-        [ ">"  , TokenKind::Greater    ]
-        [ "<"  , TokenKind::Less       ]
+        [ input, expected                ]
+        [ "{"  , TokenKind::LeftBrace    ]
+        [ "}"  , TokenKind::RightBrace   ]
+        [ "("  , TokenKind::LeftParen    ]
+        [ ")"  , TokenKind::RightParen   ]
+        [ "."  , TokenKind::Dot          ]
+        [ ":"  , TokenKind::Colon        ]
+        [ ";"  , TokenKind::SemiColon    ]
+        [ "!"  , TokenKind::Bang         ]
+        [ "+"  , TokenKind::Plus         ]
+        [ "-"  , TokenKind::Minus        ]
+        [ "*"  , TokenKind::Star         ]
+        [ "/"  , TokenKind::Slash        ]
+        [ "="  , TokenKind::Equal        ]
+        [ ">"  , TokenKind::Greater      ]
+        [ "<"  , TokenKind::Less         ]
+        [ "->" , TokenKind::RightArrow   ]
+        [ "<=" , TokenKind::LessEqual    ]
+        [ ">=" , TokenKind::GreaterEqual ]
+        [ "==" , TokenKind::EqualEqual   ]
+        [ "!=" , TokenKind::BangEqual    ]
+        [ "&&" , TokenKind::And          ]
+        [ "||" , TokenKind::Or           ]
     )]
-    fn can_lex_single_char_tokens(input: &str, expected: TokenKind) {
+    fn can_lex_static_tokens(input: &str, expected: TokenKind) {
         let maybe_token = token_stream(input.chars()).next();
         match maybe_token {
-            Some(token) => assert_eq!(expected, token, "Lexed incorrect token {:?} for input {:?}.", expected, input),
+            Some(token) => assert_eq!(
+                expected, token,
+                "Lexed incorrect token {:?} for input {:?}.", expected, input
+            ),
             None => panic!("No token returned for input {:?}", input),
         };
     }
