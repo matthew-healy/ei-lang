6c253f14b2ddb843083d320add31cf6bac757e03
Matthew Healy <matthew.healy@soundcloud.com>
2022-01-18 10:13:44 +0100

    Interpret function application
    It's a bit rich to say this is interpreting function application. It
    works only for the print_ln function, because making it work more
    generally for native functions would have required solving some lifetime
    puzzles I really don't want to bother with right now.

--- src/ast/src/lib.rs
+++ src/ast/src/lib.rs
@@ -6,6 +6,14 @@ pub struct UntypedProgram {
 }
 
 impl UntypedProgram {
+    pub fn empty() -> UntypedProgram {
+        UntypedProgram { stmts: Vec::new() }
+    }
+
+    pub fn with_stmts(stmts: Vec<Stmt>) -> UntypedProgram {
+        UntypedProgram { stmts }
+    }
+
     pub fn pretty_printed(&self) -> String {
         format!("#{:#?}", self)
     }
@@ -23,9 +31,38 @@ pub enum Expr {
     FunctionApplication { callee: Box<Expr>, args: Vec<Expr> },
 }
 
+impl Expr {
+    pub fn identifier<S: Into<String>>(s: S) -> Expr {
+        Expr::Identifier {
+            name: Token::identifier(s.into()),
+        }
+    }
+}
+
+// TODO: this is a bad idea, since at the callsite
+// Expr::from("a") could reasonably be expected to produce
+// an identifier rather than a literal.
 impl<T: Into<Literal>> From<T> for Expr {
     fn from(l: T) -> Expr {
         Expr::Literal { l: l.into() }
+    }
+}
+
+pub trait ExprVisitor<V> {
+    fn visit_identifier(&mut self, name: &Token) -> V;
+    fn visit_literal(&mut self, l: &Literal) -> V;
+    fn visit_function_application(&mut self, callee: &Expr, args: &[Expr]) -> V;
+}
+
+impl Expr {
+    pub fn accept<Val, Visitor: ExprVisitor<Val>>(&self, visitor: &mut Visitor) -> Val {
+        match self {
+            Expr::Identifier { name } => visitor.visit_identifier(name),
+            Expr::Literal { l } => visitor.visit_literal(l),
+            Expr::FunctionApplication { callee, args } => {
+                visitor.visit_function_application(callee, args)
+            }
+        }
     }
 }
 
--- src/interpreter/src/lib.rs
+++ src/interpreter/src/lib.rs
@@ -1,1 +1,131 @@
+use ast::{Expr, ExprVisitor, Literal, UntypedProgram};
+use token::Token;
 
+#[derive(Debug)]
+enum Value {
+    PrintFn,
+    String(String),
+    Void,
+}
+
+struct Interpreter<'w, W: std::fmt::Write> {
+    out: &'w mut W,
+}
+
+impl<'w, W: std::fmt::Write> Interpreter<'w, W> {
+    fn new(out: &mut W) -> Interpreter<W> {
+        Interpreter { out }
+    }
+
+    fn interpret(&mut self, p: UntypedProgram) {
+        for s in p.stmts {
+            match s {
+                ast::Stmt::Expr { e } => {
+                    e.accept(self);
+                    ()
+                }
+            }
+        }
+    }
+}
+
+impl<'w, W: std::fmt::Write> ExprVisitor<Value> for Interpreter<'w, W> {
+    fn visit_identifier(&mut self, name: &Token) -> Value {
+        if name.lexeme == "print_ln" {
+            Value::PrintFn
+        } else {
+            panic!("Unknown identifier: {}", name.lexeme)
+        }
+    }
+
+    fn visit_literal(&mut self, l: &Literal) -> Value {
+        match l {
+            // TODO(STR_TABLE): avoid this clone
+            Literal::String(s) => Value::String(s.clone()),
+        }
+    }
+
+    fn visit_function_application(&mut self, callee: &Expr, args: &[Expr]) -> Value {
+        let func = callee.accept(self);
+        match func {
+            Value::PrintFn => {
+                if args.len() == 1 {
+                    let arg_value = args[0].accept(self);
+                    match arg_value {
+                        Value::String(s) => {
+                            writeln!(self.out, "{}", s).expect("Failed to write to stdout");
+                            Value::Void
+                        }
+                        _ => panic!("Expected a String argument to print_ln."),
+                    }
+                } else {
+                    panic!(
+                        "Wrong number of args to print_ln function. Expected 1, got: {}",
+                        args.len()
+                    )
+                }
+            }
+            v => panic!("Cannot call a non-function. Tried to call: {:?}", v),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ast::{Expr, Stmt};
+    use test_with_parameters::*;
+
+    struct FakeWrite {
+        written: Vec<String>,
+    }
+
+    impl FakeWrite {
+        fn new() -> FakeWrite {
+            FakeWrite {
+                written: Vec::new(),
+            }
+        }
+    }
+
+    impl std::fmt::Write for FakeWrite {
+        fn write_str(&mut self, s: &str) -> std::fmt::Result {
+            self.written.push(s.to_owned());
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn empty_program_performs_no_io() {
+        let mut write = FakeWrite::new();
+        let mut interpreter = Interpreter::new(&mut write);
+
+        let empty_program = UntypedProgram::empty();
+        interpreter.interpret(empty_program);
+
+        assert!(write.written.is_empty())
+    }
+
+    #[test_with_parameters(
+        [ string_to_print, expected_output ]
+        [ ""             , ""              ]
+        [ "hi wrld"      , "hi wrld"       ]
+    )]
+    fn println_outputs_arg(string_to_print: &str, expected_output: &str) {
+        let (input, output) = (string_to_print.to_owned(), expected_output.to_owned());
+
+        let program = UntypedProgram::with_stmts(vec![Stmt::Expr {
+            e: Expr::FunctionApplication {
+                callee: Box::new(Expr::identifier("print_ln")),
+                args: vec![input.into()],
+            },
+        }]);
+
+        let mut write = FakeWrite::new();
+        let mut interpreter = Interpreter::new(&mut write);
+
+        interpreter.interpret(program);
+
+        assert_eq!(write.written, vec![output, "\n".to_string()])
+    }
+}
