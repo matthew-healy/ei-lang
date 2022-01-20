3a8eed0eac28451bbbf1cf7134b53b7fc35e7a08
Matthew Healy <matthew.healy@soundcloud.com>
2022-01-20 11:17:02 +0100

    Generalise native functions

--- src/interpreter/src/lib.rs
+++ src/interpreter/src/lib.rs
@@ -1,20 +1,64 @@
+use std::{cell::RefCell, collections::HashMap, io::Write, rc::Rc};
+
 use ast::{Expr, ExprVisitor, Literal, UntypedProgram};
 use token::Token;
 
+struct NativeFn {
+    body: Box<dyn FnMut(&[Value]) -> Value>,
+}
+
+struct Globals {
+    ns: HashMap<String, NativeFn>,
+}
+
+impl<'program> Globals {
+    fn new() -> Globals {
+        Globals { ns: HashMap::new() }
+    }
+
+    fn add<S: Into<String>>(&mut self, k: S, v: NativeFn) {
+        self.ns.insert(k.into(), v);
+    }
+
+    fn call(&mut self, name: &str, args: &[Value]) -> Value {
+        let func = self
+            .ns
+            .get_mut(name)
+            .expect(&format!("No such function {}", name));
+        (func.body)(args)
+    }
+}
+
 #[derive(Debug)]
 enum Value {
-    PrintFn,
+    GlobalFn(String),
     String(String),
     Void,
 }
 
-struct Interpreter<'w, W: std::fmt::Write> {
-    out: &'w mut W,
+struct Interpreter {
+    globals: Globals,
 }
 
-impl<'w, W: std::fmt::Write> Interpreter<'w, W> {
-    fn new(out: &mut W) -> Interpreter<W> {
-        Interpreter { out }
+impl Interpreter {
+    fn new<W: Write + 'static>(write: Rc<RefCell<W>>) -> Interpreter {
+        let mut globals = Globals::new();
+
+        globals.add(
+            "print_ln",
+            NativeFn {
+                body: Box::new(move |args| {
+                    let s = match args {
+                        [Value::String(s)] => s,
+                        _ => panic!("Incorrect arguments for print_ln"),
+                    };
+                    writeln!(write.borrow_mut(), "{}", s).expect("Oh no!");
+                    Value::Void
+                }),
+            },
+        );
+
+        Interpreter { globals }
     }
 
     fn interpret(&mut self, p: UntypedProgram) {
@@ -29,13 +73,9 @@ impl<'w, W: std::fmt::Write> Interpreter<'w, W> {
     }
 }
 
-impl<'w, W: std::fmt::Write> ExprVisitor<Value> for Interpreter<'w, W> {
+impl ExprVisitor<Value> for Interpreter {
     fn visit_identifier(&mut self, name: &Token) -> Value {
-        if name.lexeme == "print_ln" {
-            Value::PrintFn
-        } else {
-            panic!("Unknown identifier: {}", name.lexeme)
-        }
+        Value::GlobalFn(name.lexeme.clone())
     }
 
     fn visit_literal(&mut self, l: &Literal) -> Value {
@@ -48,22 +88,9 @@ impl<'w, W: std::fmt::Write> ExprVisitor<Value> for Interpreter<'w, W> {
     fn visit_function_application(&mut self, callee: &Expr, args: &[Expr]) -> Value {
         let func = callee.accept(self);
         match func {
-            Value::PrintFn => {
-                if args.len() == 1 {
-                    let arg_value = args[0].accept(self);
-                    match arg_value {
-                        Value::String(s) => {
-                            writeln!(self.out, "{}", s).expect("Failed to write to stdout");
-                            Value::Void
-                        }
-                        _ => panic!("Expected a String argument to print_ln."),
-                    }
-                } else {
-                    panic!(
-                        "Wrong number of args to print_ln function. Expected 1, got: {}",
-                        args.len()
-                    )
-                }
+            Value::GlobalFn(name) => {
+                let arg_vals: Vec<Value> = args.iter().map(|e| e.accept(self)).collect();
+                self.globals.call(&name, &arg_vals)
             }
             v => panic!("Cannot call a non-function. Tried to call: {:?}", v),
         }
@@ -72,6 +99,8 @@ impl<'w, W: std::fmt::Write> ExprVisitor<Value> for Interpreter<'w, W> {
 
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
     use super::*;
     use ast::{Expr, Stmt};
     use test_with_parameters::*;
@@ -88,30 +117,38 @@ mod tests {
         }
     }
 
-    impl std::fmt::Write for FakeWrite {
-        fn write_str(&mut self, s: &str) -> std::fmt::Result {
+    impl Write for FakeWrite {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            let s = std::str::from_utf8(buf).unwrap();
             self.written.push(s.to_owned());
-            Ok(())
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            // Ok(())
+            todo!()
         }
     }
 
     #[test]
     fn empty_program_performs_no_io() {
-        let mut write = FakeWrite::new();
-        let mut interpreter = Interpreter::new(&mut write);
+        let write = Rc::new(RefCell::new(FakeWrite::new()));
+        let our_write = Rc::clone(&write);
+
+        let mut interpreter = Interpreter::new(write);
 
         let empty_program = UntypedProgram::empty();
         interpreter.interpret(empty_program);
 
-        assert!(write.written.is_empty())
+        assert!(our_write.borrow().written.is_empty())
     }
 
     #[test_with_parameters(
-        [ string_to_print, expected_output ]
-        [ ""             , ""              ]
-        [ "hi wrld"      , "hi wrld"       ]
+        [ string_to_print, expected_output       ]
+        [ ""             , &["\n"]            ]
+        [ "hi wrld"      , &["hi wrld", "\n"] ]
     )]
-    fn println_outputs_arg(string_to_print: &str, expected_output: &str) {
+    fn println_outputs_arg(string_to_print: &str, expected_output: &[&str]) {
         let (input, output) = (string_to_print.to_owned(), expected_output.to_owned());
 
         let program = UntypedProgram::with_stmts(vec![Stmt::Expr {
@@ -121,11 +158,14 @@ mod tests {
             },
         }]);
 
-        let mut write = FakeWrite::new();
-        let mut interpreter = Interpreter::new(&mut write);
+        let write = Rc::new(RefCell::new(FakeWrite::new()));
+        let our_write = Rc::clone(&write);
+
+        let mut interpreter = Interpreter::new(write);
 
         interpreter.interpret(program);
 
-        assert_eq!(write.written, vec![output, "\n".to_string()])
+        let written = our_write.borrow().written.clone();
+        assert_eq!(written, output)
     }
 }
